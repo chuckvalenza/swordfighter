18e0d3080b75a5b1539eae6838ac90236029722a
Chuck Valenza <fallencx@gmail.com>
2017-01-08 21:07:51 -0500

    Add the initialization files

---
+++ jni/src/core_components/swordfighter.cpp
@@ -0,0 +1,38 @@
+/**
+ * swordfighter.cpp
+ *
+ * Description: Creates the game class and loads resources. Can eventually be
+ * used to load the menu.
+ */
+
+#include "core_components/swordfighter.h"
+
+#include "oxygine-framework.h"
+#include "core_components/res.h"
+#include "core_components/Game.h"
+
+using namespace oxygine;
+
+void swordfighter_preinit()
+{
+}
+
+void swordfighter_init()
+{
+	// load resources? swordfighter is vague. not exactly sure what this does.
+	res::load();
+
+	// create our game
+	spGame game = new Game;
+	game->init();
+	game->attachTo(getStage());
+}
+
+void swordfighter_update()
+{
+}
+
+void swordfighter_destroy()
+{
+	res::free();
+}
---
+++ jni/src/core_components/swordfighter.h
@@ -0,0 +1,16 @@
+/**
+ * swordfighter.h
+ *
+ * Description: Creates the game class and loads resources. Can eventually be
+ * used to load the menu.
+ */
+
+#ifndef SWORD_FIGHTER_SWORDFIGHTER_H
+#define SWORD_FIGHTER_SWORDFIGHTER_H
+
+void swordfighter_preinit();
+void swordfighter_init();
+void swordfighter_update();
+void swordfighter_destroy();
+
+#endif //SWORD_FIGHTER_SWORDFIGHTER_H
