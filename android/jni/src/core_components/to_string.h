d5b3ba19854d697ed5265c0228d5212db1f06ac7
Chuck Valenza <fallencx@gmail.com>
2018-03-21 23:23:08 -0700

    Add to_c_str function to to_string.h

--- android/jni/src/core_components/to_string.h
+++ android/jni/src/core_components/to_string.h
@@ -13,9 +13,16 @@
 template <typename T>
 std::string to_string(T value)
 {
-    std::ostringstream os ;
-    os << value ;
-    return os.str() ;
+    std::ostringstream os;
+    os << value;
+    return os.str();
+}
+
+template <typename T>
+const char* to_c_string(T value)
+{
+	std::string s = to_string(value);
+	return to_string(value).c_str();
 }
 
 #endif //SWORD_FIGHTER_TO_STRING_H
